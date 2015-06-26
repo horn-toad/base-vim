8edd66e7c0a2a4a99678902c6e0271d1a33f78a7
horntoad <jeremy@horntoad.ws>
2015-06-26 12:11:58 +0000

    push backups into a .old dir

--- install.sh
+++ install.sh
@@ -6,14 +6,16 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 cd ~
 if [[ -d ~/.vim ]]
 then
-  mv ~/.vim ~/.vim.old.$current_time
-  mv ~/.vimrc ~/.vimrc.old.$current_time
+  mkdir -p .old
+  mv ~/.vim ~/.old/vim.$current_time
+  mv ~/.vimrc ~/.old/vimrc.$current_time
 fi
 
 if [[ -d ~/_vim ]]
 then
-  mv ~/_vim ~/_vim.old.$current_time
-  mv ~/_vimrc ~/_vimrc.old.$current_time
+  mkdir -p _old
+  mv ~/_vim ~/_old/_vim.$current_time
+  mv ~/_vimrc ~/_old/_vimrc.$current_time
 fi
 
 git clone https://github.com/horn-toad/base-vim.git ~/.vim
